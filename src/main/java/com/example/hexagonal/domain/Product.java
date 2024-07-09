cf2f04ee3b293f1821d6840600115b0b316c5b3e
vablas <vablas@minsait.com>
2024-07-09 12:15:10 +0200

    Ejercicio Dia 1

---
+++ src/main/java/com/example/hexagonal/domain/Product.java
@@ -0,0 +1,42 @@
+package com.example.hexagonal.domain;
+
+public class Product {
+
+    private long id;
+    private String name;
+    private Double price;
+
+    public Product() {
+
+    }
+
+    public Product(long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
